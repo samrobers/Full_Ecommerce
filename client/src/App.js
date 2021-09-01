af23543182c2dd0c46260e2d40617bebfd53c178
samrobers <sam.roberson32@gmail.com>
2021-09-01 15:53:21 -0600

    have basic bootstrap template in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Header />
+      <Router>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+      </Router>
+      <Footer />
+    </ApolloProvider>
   );
 }
 
